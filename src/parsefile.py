6611983e6a220e718a18888e80d0cc53bdc00a95
Thomas Ferdinand M <56783537+thomas-fm@users.noreply.github.com>
2020-11-16 04:12:34 +0700

    html similarity more accurate

--- src/parsefile.py
+++ src/parsefile.py
@@ -6,4 +6,24 @@ def htmlParser(filename):
   html_doc = codecs.open("./static/docs/" + filename, "r", "utf-8")
   soup = BeautifulSoup(html_doc, 'html.parser')
 
-  return soup.get_text()
+  parse_string = []
+
+  for i in range (0, len(soup.find_all('p'))):
+    text = soup.find_all('p')[i].get_text()
+  
+    if (len(text.split(' ')) > 6 and '.' in text):
+      parse_string.append(text)
+  parse_string = ' '.join(parse_string)
+  
+  return parse_string
+
+def htmlFirst(filename):
+  html_doc = codecs.open("./static/docs/" + filename, "r")
+  soup = BeautifulSoup(html_doc, 'html.parser')
+
+  for i in range (0, len(soup.find_all('p'))):
+    text = soup.find_all('p')[i].get_text()
+  
+    if (len(text.split('.')) > 3 and '.' in text):
+      return text.split('.')[0]
+  return None
